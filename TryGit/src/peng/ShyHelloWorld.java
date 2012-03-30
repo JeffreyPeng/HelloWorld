fe3dfac7bb2466a0de83174ddc2a81702d069e94
Jeffrey Peng <pyx718@gmail.com>
2012-03-31 02:09:01 +0800

    initial commit

---
+++ TryGit/src/peng/ShyHelloWorld.java
@@ -0,0 +1,13 @@
+package peng;
+
+public class ShyHelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hi");
+	}
+
+}
